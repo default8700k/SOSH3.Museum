6fffe65966d99636c47a64c88df5d22bd3803363
Victor Maltsev <default8700k@gmail.com>
2022-02-27 01:37:41 +0300

    Add SOSH3.Museum.WebApplication.Tests files.

---
+++ tests/SOSH3.Museum.WebApplication.Tests/Controllers/HomeControllerIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace SOSH3.Museum.WebApplication.Tests.Controllers
+{
+    public class HomeControllerIntegrationTests : IClassFixture<WebApplicationTestFactory>
+    {
+        private readonly WebApplicationTestFactory factory;
+
+        public HomeControllerIntegrationTests(WebApplicationTestFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task Index_ShouldBeCorrect()
+        {
+            // setup
+            // nothing
+
+            // act
+            var result = await factory.HttpClient.GetAsync("/");
+
+            // assert
+            result.Should().Be200Ok();
+            result.Content.Headers.ContentType.Should().BeEquivalentTo(
+                new
+                {
+                    CharSet = "utf-8",
+                    MediaType = "text/html"
+                }
+            );
+        }
+    }
+}
---
+++ tests/SOSH3.Museum.WebApplication.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions.AspNetCore.Mvc;
+using SOSH3.Museum.WebApplication.Controllers;
+using Xunit;
+
+namespace SOSH3.Museum.WebApplication.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController target;
+
+        public HomeControllerTests()
+        {
+            this.target = new HomeController();
+        }
+
+        [Theory]
+        [AutoData]
+        public void Index_ShouldBeViewResult(string page)
+        {
+            // setup
+            // nothing
+
+            // act
+            var result = target.Index(page);
+
+            // assert
+            result.Should().BeViewResult();
+        }
+    }
+}
