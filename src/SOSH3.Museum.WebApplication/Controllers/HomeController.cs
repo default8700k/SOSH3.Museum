2659e71f0e33c64a8dcae83608c71592a9f4c302
Victor Maltsev <default8700k@gmail.com>
2022-02-27 01:14:37 +0300

    Add SOSH3.Museum.WebApplication files.

---
+++ src/SOSH3.Museum.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SOSH3.Museum.WebApplication.ViewModels;
+
+namespace SOSH3.Museum.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string page)
+        {
+            return View(
+                new IndexViewModel
+                {
+                    Robots = "noindex, nofollow",
+                    Title = "title",
+                    Description = "no description",
+                    Content = $"page: {page}"
+                }
+            );
+        }
+    }
+}
